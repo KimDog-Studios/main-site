bc43d707959840649431b199f918749becc84b8d
KimDog-Studios <kim.dog.543@gmail.com>
2024-07-27 16:19:07 +0100

    Update

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,14 @@
+import { TypingEffectHomePage } from "@/components/[API]MainFunctions";
 import { modsData, CategorySection } from "@/components/[API]Mods";
+import NavBar from "@/components/[API]NavBar";
 
 
 // Page component
 const Page: React.FC = () => {
   return (
     <div className="relative min-h-screen bg-black">
+        <TypingEffectHomePage />
+        <NavBar />
       <main className="flex flex-col items-center justify-center min-h-screen text-center">
         <h1 className="text-4xl font-bold mb-8 text-white">This will be the New Home Page</h1>
         {Object.entries(modsData).map(([category, mods]) => (
